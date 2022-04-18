0b671ac942cca39131ec6e02a2778dc0c88cfac1
Maciej Błędkowski <pub@mble.dk>
2022-04-18 12:01:04 +0000

    Added getDownloadUrl to mojang

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,6 +1,9 @@
 import version from '../components/prompts/version';
 import server from '../components/prompts/server';
+import {getDownloadUrl} from '../connections/mojang';
 
 export default async function prompt() {
   await server(await version());
+  const url = await getDownloadUrl("1.18.2");
+  console.log(url);
 };
--- src/connections/mojang.ts
+++ src/connections/mojang.ts
@@ -10,5 +10,21 @@ async function getVersions() {
   }
 }
 
+async function getDownloadUrl(version: string) {
+  try {
+    const response = await axios.get(`https://launchermeta.mojang.com/mc/game/version_manifest_v2.json`);
+    const versions = response.data.versions;
+    const versionData = versions.find((v: { id: string; }) => v.id === version);
+    if (!versionData) {
+      throw new Error(`Version ${version} not found`);
+    }
+    const versionResponse = await axios.get(versionData.url);
+    const downloadLink = versionResponse.data.downloads.server.url;
+    return downloadLink;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
 
-export {getVersions};
+export {getVersions, getDownloadUrl};
