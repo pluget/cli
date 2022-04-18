c80e415dc0c06daa410c35d192c8c1f7046c3666
Maciej Błędkowski <pub@mble.dk>
2022-04-18 14:31:43 +0000

    Moved help message to separate files.

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,12 +1,60 @@
+import { readFile } from 'fs';
+import { join } from 'path';
+
 export default async function help(command?: string) {
-  console.info("Minecraft Command Line Interface (mcli)");
-  console.info("Usage: mcli [command] [flags]");
-  console.info("Commands:");
-  console.info(" - add: Add a new plugin to package.json");
-  console.info(" - init: Initialize a new mcli project");
-  console.info(" - install: Install server and all plugins");
-  console.info(" - upgrade: Upgrade server and plugins");
-  console.info(" - search: Search for plugins");
-  console.info(" - help: Show this help message");
-  console.info("Run 'mcli help [command]' for more information on specific command");
+  switch (command) {
+    case 'init':
+      readFile(join(__dirname, '/../views/help/init.txt'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+      break;
+    case 'add':
+      readFile(join(__dirname, '/../views/help/add.txt'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+      break;
+    case 'install':
+      readFile(join(__dirname, '/../views/help/install.txt'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+      break;
+    case 'upgrade':
+      readFile(join(__dirname, '/../views/help/upgrade.txt'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+      break;
+    case 'search':
+      readFile(join(__dirname, '/../views/help/search.txt'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+      break;
+    default:
+      readFile(join(__dirname, '/../views/help/index.txt'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+  }
 }
