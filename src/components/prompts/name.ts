e03f097c82cc9103c3d154b81a8122daa4ec0cb9
Maciej Błędkowski <pub@mble.dk>
2022-04-30 00:40:32 +0200

    Updated name.ts

--- src/components/prompts/name.ts
+++ src/components/prompts/name.ts
@@ -4,13 +4,7 @@ export default function name() {
   return prompts({
     type: 'text',
     name: 'value',
-    message: 'What is the name of your project?',
-    initial: 'MyProject',
-    validate: (value) => {
-      if (value.length) {
-        return true;
-      }
-      return 'Please enter a name';
-    }
+    message: 'What is the name of your server?',
+    initial: 'my-mpm-server',
   });
 };
