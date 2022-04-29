7c5f41c444be0fbf5681398b938a3016000860c1
Maciej Błędkowski <pub@mble.dk>
2022-04-30 00:47:18 +0200

    Updated errors in createServer.ts

--- src/components/createServer.ts
+++ src/components/createServer.ts
@@ -3,12 +3,19 @@ import fse from 'fs-extra';
 import { join } from 'path';
 
 export default async function createServer(path: string, projectName: string) {
-  const dir = join(process.cwd(), path, projectName);
-  await fs.access(dir, (err) => {
+  const parentDir = join(process.cwd(), path)
+  const dir = join(parentDir, projectName);
+  const checkParentDirectoryExists = fs.access(parentDir, (err) => {
+    if (err) {
+      throw new Error(`The directory ${parentDir} does not exist.`);
+    }
+  });
+  const checkProjectExist = fs.access(dir, (err) => {
     if (!err) {
       throw new Error(`Directory ${dir} already exists`);
     }
   });
+  await Promise.all([checkParentDirectoryExists, checkProjectExist]);
   await fse.mkdir(dir);
   await fse.copy(join(__dirname, '..', 'templates', 'server'), dir);
 }
