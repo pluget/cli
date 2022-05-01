34c0eca10627c9f032b906bf011c91e4f63cf3b3
Maciej Błędkowski <pub@mble.dk>
2022-05-01 10:56:27 +0200

    Update to createServer, created installServer

--- src/components/createServer.ts
+++ src/components/createServer.ts
@@ -1,21 +1,37 @@
 import fs from 'fs';
 import fse from 'fs-extra';
-import { join } from 'path';
+import { join, resolve } from 'path';
+import installServer from './installServer';
 
 export default async function createServer(path: string, projectName: string) {
-  const parentDir = join(process.cwd(), path)
+  const parentDir = resolve(process.cwd(), path)
   const dir = join(parentDir, projectName);
-  const checkParentDirectoryExists = fs.access(parentDir, (err) => {
-    if (err) {
-      throw new Error(`The directory ${parentDir} does not exist.`);
-    }
-  });
-  const checkProjectExist = fs.access(dir, (err) => {
-    if (!err) {
-      throw new Error(`Directory ${dir} already exists`);
-    }
-  });
-  await Promise.all([checkParentDirectoryExists, checkProjectExist]);
+
+  await Promise.all([
+    fs.access(parentDir, (err) => {
+      if (err) {
+        throw new Error(`The directory ${parentDir} does not exist.`);
+      }
+    }),
+    fs.access(dir, (err) => {
+      if (!err) {
+        throw new Error(`Directory ${dir} already exists`);
+      }
+    })
+  ]);
+
   await fse.mkdir(dir);
-  await fse.copy(join(__dirname, '..', 'templates', 'server'), dir);
+
+  await Promise.all([
+    fse.copy(resolve(__dirname, '../templates/createServer'), dir),
+    fse.ensureDir(resolve(dir, './logs/logs')),
+    fse.ensureDir(resolve(dir, './plugins')),
+    fse.ensureDir(resolve(dir, './worlds/world')),
+    fse.ensureDir(resolve(dir, './worlds/world_nether')),
+    fse.ensureDir(resolve(dir, './worlds/world_the_end')),
+    fse.ensureDir(resolve(dir, './modules/server')),
+    fse.ensureDir(resolve(dir, './modules/plugins')),
+  ]);
+
+  await installServer(dir);
 }
---
+++ src/components/installServer.ts
@@ -0,0 +1,40 @@
+import fse from "fs-extra";
+import { join, resolve } from "path";
+
+export default async function installServer(path: string) {
+  await fse.copy(resolve(__dirname, '../templates/installServer'), path)
+
+  const dirs = {} as {
+    [key: string]: {
+      path: string,
+      dest: string,
+      files: string[],
+    }
+  };
+
+  dirs["configDir"] = { path: "./config", dest: "./bin", files: [] };
+  dirs["logsDir"] = { path: "./logs", dest: "./bin", files: [] };
+  dirs["worldsDir"] = { path: "./worlds", dest: "./bin", files: [] };
+  dirs["modulesServerDir"] = { path: "./modules/server", dest: "./bin", files: [] };
+  dirs["modulesPluginsDir"] = { path: "./modules/plugins", dest: "./plugins", files: [] };
+  [dirs["configDir"].files, dirs["logsDir"].files, dirs["worldsDir"].files, dirs["modulesServerDir"].files, dirs["modulesPluginsDir"].files] = await Promise.all([
+    fse.readdir(resolve(path, "./config")),
+    fse.readdir(resolve(path, "./logs")),
+    fse.readdir(resolve(path, "./worlds")),
+    fse.readdir(resolve(path, "./modules/server")),
+    fse.readdir(resolve(path, "./modules/plugins"))
+  ]);
+
+  const symlinkPromises = [] as Promise<void>[];
+
+  for (const dir in dirs) {
+    for (const file of dirs[dir].files) {
+      symlinkPromises.push(fse.symlink(join('..', dirs[dir].path, file), resolve(path, './bin', file)));
+    }
+  };
+
+  await Promise.all([
+    fse.symlink('../plugins', resolve(path, './bin/plugins'), 'dir'),
+    ...symlinkPromises
+  ]);
+}
