308e4136466156a3e5874606735f036e4573adf8
Maciej Błędkowski <pub@mble.dk>
2022-05-03 18:53:12 +0200

    Small refactor of index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,19 @@ var args = minimist(process.argv.slice(2));
 import init from "./commands/init";
 import help from "./commands/help";
 
+function catchedError(err: Error): void {
+  console.error(err);
+  process.exit(1);
+}
+
 if (args._.length === 2 && args._[0] === "help") {
-  help(args._[1]).catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+  help(args._[1]).catch(catchedError);
 } else {
   switch (args._[0]) {
     case "init":
-      init(args._[1] || "").catch((err) => {
-        console.error(err);
-        process.exit(1);
-      });
+      init(args._[1] || "").catch(catchedError);
       break;
     default:
-      help().catch((err) => {
-        console.error(err);
-        process.exit(1);
-      });
+      help().catch(catchedError);
   }
 }
