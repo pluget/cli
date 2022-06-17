0f2c62b3324dc3afe3d156e66336f61878de1d1d
Maciej Błędkowski <pub@mble.dk>
2022-06-17 11:52:39 +0200

    Update to install

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -1,5 +1,11 @@
+import { resolve } from "path";
 import installServer from "../components/installServer";
+import packageJson from "../interfaces/packageJson";
 
 export default async function install(path: string) {
-  await installServer(path);
+  const dir = resolve(process.cwd(), path);
+  const packageJson: packageJson = require(resolve(dir, "package.json"));
+  for (const [name, version] of Object.entries(packageJson.server)) {
+    await installServer(name, version, dir);
+  }
 }
--- src/components/installServer.ts
+++ src/components/installServer.ts
@@ -9,59 +9,62 @@ import {
   getDownloadUrl as getPaperDownloadUrl,
   getDownloadUrlForSpecificBuild as getPaperDownloadUrlForSpecificBuild,
 } from "../connections/papermc";
-import packageJson from "../interfaces/packageJsonInterface";
+import packageJson from "../interfaces/packageJson";
 import downloadServer from "./downloadServer";
 import createDebugMessages from "debug";
-const debug = createDebugMessages("debugging")
+const debug = createDebugMessages("debugging");
 
-async function getDownloadUrl(packageJson: packageJson) {
+async function getDownloadUrl(name: string, version: string) {
   let downloadUrl = "";
-  for (const server in packageJson.server) {
-    debug(server);
-    const [serverVersion, serverBuild] = packageJson.server[server].split("-");
-    debug(serverVersion, serverBuild)
-    switch (server) {
-      case "glowstone":
-        throw Error("Glowestone is not implemented yet");
-      case "paper":
-        debug("switch - paper")
-        if (serverBuild !== undefined) {
-          downloadUrl =
-            (await getPaperDownloadUrlForSpecificBuild(
-              serverVersion,
-              parseInt(serverBuild)
-            )) || "";
-        } else {
-          downloadUrl = (await getPaperDownloadUrl(serverVersion)) || "";
-        }
-        break;
-      case "spigot":
-        downloadUrl = await getSpigotDownloadUrl(serverVersion);
-        break;
-      case "bukkit":
-        downloadUrl = await getBukkitDownloadUrl(serverVersion);
-        break;
-      case "vanilla":
-        downloadUrl = await getVanillaDownloadUrl(serverVersion);
-        break;
-    }
+  debug(name);
+  const [serverVersion, serverBuild] = version.split("-");
+  debug(serverVersion, serverBuild);
+  switch (name) {
+    case "glowstone":
+      throw Error("Glowestone is not implemented yet");
+    case "paper":
+      debug("switch - paper");
+      if (serverBuild !== undefined) {
+        downloadUrl =
+          (await getPaperDownloadUrlForSpecificBuild(
+            serverVersion,
+            parseInt(serverBuild)
+          )) || "";
+      } else {
+        downloadUrl = (await getPaperDownloadUrl(serverVersion)) || "";
+      }
+      break;
+    case "spigot":
+      downloadUrl = await getSpigotDownloadUrl(serverVersion);
+      break;
+    case "bukkit":
+      downloadUrl = await getBukkitDownloadUrl(serverVersion);
+      break;
+    case "vanilla":
+      downloadUrl = await getVanillaDownloadUrl(serverVersion);
+      break;
   }
-  return downloadUrl
+  return downloadUrl;
 }
 
-export default async function installServer(path: string) {
-  const packageJsonPath = resolve(path, "./package.json");
-  const packageJson: packageJson = await fse.readJson(packageJsonPath);
-
-  const downloadUrl = await getDownloadUrl(packageJson);
+export default async function installServer(
+  name: string,
+  version: string,
+  path: string
+) {
+  const downloadUrl = await getDownloadUrl(name, version);
 
   debug(downloadUrl);
 
   const modulesServerPath = resolve(path, "./modules/server/");
   fse.ensureDir(modulesServerPath);
   fse.copy(
-    await downloadServer(downloadUrl, "server"), resolve(modulesServerPath, "./server.jar")
+    await downloadServer(downloadUrl, "server"),
+    resolve(modulesServerPath, "./server.jar")
   );
-  fse.ensureDir(resolve(path, "./bin"))
-  fse.symlink(resolve(path, "./bin/server.jar"), resolve(modulesServerPath, "./server.jar"));
+  fse.ensureDir(resolve(path, "./bin"));
+  fse.symlink(
+    resolve(path, "./bin/server.jar"),
+    resolve(modulesServerPath, "./server.jar")
+  );
 }
